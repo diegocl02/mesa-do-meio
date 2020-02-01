cee58eff095cfb0538b06a7fb88f594637f5a31d
Jesus rey <diegocl02@gmail.com>
2020-02-01 03:39:43 -0300

    Created board component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import Test from './test'
+import {Board} from './board'
 
 function App() {
   return (
     <div className="App">
       <Test></Test>
+      <Board></Board>
     </div>
   );
 }
