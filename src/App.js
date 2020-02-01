8f8e6b057e2fb2a6430dbdcccab1e25999cea172
PooWoox <rdemellobrochado@gmail.com>
2020-02-01 02:26:36 -0300

    Player component initial commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import './App.css';
 
+import Player from "./components/player"
+
 function App() {
   return (
     <div className="App">
       {"Just testing..."}
+      <Player />
     </div>
   );
 }
---
+++ src/components/player/index.js
@@ -0,0 +1,48 @@
+import React, {Component} from 'react'
+import { UP, DOWN, LEFT, RIGHT } from '../../helpers/constants'
+
+class Player extends Component {
+    // constructor(props) {
+    //     this.state= {
+    //         direction: newDirection
+    //     }
+    // }
+
+    handleKeyDown = (e) => {
+        let newDirection;
+        
+        switch(e.keyCode) {
+            case 37:
+                newDirection = { top: 0, left: -1 , dir: LEFT};
+                break;
+            case 38:
+                newDirection = { top: -1, left: 0 , dir: UP};
+                break;
+            case 39:
+                newDirection = { top: 0, left: 1, dir: RIGHT};
+                break;
+            case 40:
+                newDirection = { top: 1, left: 0, dir: DOWN };
+                break;
+            default:
+                return;
+        }
+
+        // this.props.handlePlayerMovement(newDirection);
+        console.log(newDirection);
+    }
+
+    render() {
+        return (
+        <div>
+            <img src="https://drive.google.com/open?id=1rZ2FCF5h-XtzsM-5lOrDo25ZETM1xHwn" alt="player"></img>
+        </div>
+        )
+    }
+
+    componentDidMount() {
+        window.onkeydown = this.handleKeyDown;
+    }
+}
+
+export default Player
